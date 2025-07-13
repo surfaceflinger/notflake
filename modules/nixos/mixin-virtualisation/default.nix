40ec855db7ba7889cae8b5c8e512270a3f3b50de
nat <nat@nekopon.pl>
2025-07-13 12:13:44 +0200

    modules/nixos/mixin-virtualisation: drop modprobe config

--- modules/nixos/mixin-virtualisation/default.nix
+++ modules/nixos/mixin-virtualisation/default.nix
@@ -15,12 +15,6 @@
     };
   };
 
-  boot.extraModprobeConfig = ''
-    options kvm_amd npt=1 avic=1 nested=1 sev=1
-    options kvm ignore_msrs=1 report_ignored_msrs=0
-    options kvm_intel nested=1 emulate_invalid_guest_state=0
-  '';
-
   boot.kernelModules = [
     "vfio"
     "vfio_iommu_type1"
