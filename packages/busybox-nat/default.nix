4db2c4e6c69016ab75f3423f63f971ad85c1531c
nat <nat@nekopon.pl>
2025-08-15 16:40:13 +0200

    packages/busybox-nat: init

---
+++ packages/busybox-nat/default.nix
@@ -0,0 +1,32 @@
+{ busybox, symlinkJoin }:
+let
+  busybox' = busybox.override { enableAppletSymlinks = false; };
+in
+symlinkJoin {
+  name = "busybox-nat";
+  inherit (busybox') version;
+  paths = [ busybox' ];
+  postBuild = ''
+    tools=(
+      "ftpd"
+      "ftpget"
+      "ftpput"
+      "fuser"
+      "ifconfig"
+      "killall"
+      "lspci"
+      "lsscsi"
+      "lsusb"
+      "nc"
+      "netstat"
+      "pstree"
+      "telnet"
+      "whois"
+    )
+
+    mkdir -p $out/bin
+    for tool in "''${tools[@]}"; do
+      ln -s "${busybox}/bin/busybox" "$out/bin/$tool"
+    done
+  '';
+}
