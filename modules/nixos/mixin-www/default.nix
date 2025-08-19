1588bd60d862deec6a0891ada4a1e2340948880b
nat <nat@nekopon.pl>
2025-08-20 00:43:03 +0200

    modules/nixos/mixin-www: disable tls1.2, use tlsserver acme profile

--- modules/nixos/mixin-www/default.nix
+++ modules/nixos/mixin-www/default.nix
@@ -15,8 +15,26 @@ _: {
     email = "ssl@nekopon.pl";
     enableReload = false;
     globalConfig = ''
+      cert_issuer acme https://acme-v02.api.letsencrypt.org/directory {
+        profile tlsserver
+      }
+
       admin off
       metrics { per_host }
+    '';
+    extraConfig = ''
+      (common) {
+        tls {
+          protocols tls1.3
+        }
+
+        header {
+          ?Permissions-Policy interest-cohort=()
+          >Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+          ?X-Content-Type-Options nosniff
+          ?X-Frame-Options DENY
+        }
+      }
     '';
     virtualHosts.":2019" = {
       logFormat = "output discard";
