661f095e5dcd70a301d0e00e7c598668b5835219
nat <nat@nekopon.pl>
2025-08-22 19:37:29 +0200

    hosts/symfonisk: init

---
+++ hosts/symfonisk/configuration.nix
@@ -0,0 +1,75 @@
+{
+  config,
+  inputs,
+  nixosModules,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    "${inputs.nixos-hardware.result}/common/pc"
+    "${inputs.nixos-hardware.result}/common/pc/ssd"
+    ../../modules/nixos/desktop/networking.nix
+    ../../modules/nixos/desktop/pipewire.nix
+    nixosModules.common
+    nixosModules.mixin-telemetry
+    nixosModules.mixin-www
+    nixosModules.server
+    nixosModules.user-nat
+    ./storage.nix
+  ];
+
+  # base
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # bootloader/kernel/modules
+  hardware.enableRedistributableFirmware = true;
+  boot = {
+    loader.limine = {
+      biosDevice = "/dev/sda";
+      biosSupport = true;
+    };
+    initrd.availableKernelModules = [
+      "ehci_pci"
+      "pata_via"
+      "sd_mod"
+      "uhci_hcd"
+      "usbhid"
+      "usb_storage"
+    ];
+    kernelModules = [ "kvm-intel" ];
+  };
+  # some mitigations arent available on via eden x2 so its pointless
+  # also its literally just a music streamer
+  nix-mineral.overrides.performance.no-mitigations = true;
+
+  # pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    alsa.enable = true;
+    enable = true;
+    pulse.enable = true;
+    socketActivation = false;
+    systemWide = true;
+  };
+
+  # mopidy
+  networking.firewall.allowedTCPPorts = [
+    6680
+    8989
+  ];
+  services.mopidy = {
+    enable = true;
+    extensionPackages = [
+      pkgs.mopidy-iris
+      pkgs.mopidy-tidal
+    ];
+    configuration = ''
+      [tidal]
+      enabled = true
+      login_method = HACK
+      quality = LOSSLESS
+    '';
+  };
+  users.users.mopidy.extraGroups = [ "pipewire" ];
+}
