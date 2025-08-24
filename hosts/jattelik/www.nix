24406cd3bec7ab8da5a9b398a4b6f046b2458815
nat <nat@nekopon.pl>
2025-08-24 22:13:54 +0200

    hosts/jattelik: update caddy hash

--- hosts/jattelik/www.nix
+++ hosts/jattelik/www.nix
@@ -6,7 +6,7 @@
     environmentFile = config.age.secrets.caddy-desec-jattelik.path;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/desec@v1.0.1" ];
-      hash = "sha256-r4+WK8UhGLAuIvdV6uiH2bMh/SjTfY4CzKcpHU0Gu5s=";
+      hash = "sha256-a2GeG7TYBnCz30jBKQmmQz8Y3vutRpa+tboaahJ5xeQ=";
     };
     globalConfig = ''
       dns desec {
