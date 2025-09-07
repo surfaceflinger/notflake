91f74111f85c5b2c1eb97abdd7c08d794437b42a
nat <nat@nekopon.pl>
2025-09-07 13:20:01 +0200

    hosts/sjorapport: init

---
+++ hosts/sjorapport/configuration.nix
@@ -0,0 +1,37 @@
+{ inputs, nixosModules, ... }:
+{
+  imports = [
+    "${inputs.nixos-hardware.result}/framework/12-inch/13th-gen-intel"
+    nixosModules.common
+    nixosModules.desktop
+    nixosModules.laptop
+    nixosModules.mixin-gaming
+    nixosModules.mixin-tpm20
+    nixosModules.mixin-virtualisation
+    nixosModules.user-nat
+    ./storage.nix
+  ];
+
+  # base
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # bootloader/kernel/modules
+  hardware.enableRedistributableFirmware = true;
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [
+      "i915.force_probe=!a721" # disable i915
+      "xe.force_probe=a721" # enable xe
+      "mem_sleep_default=deep" # enable s3 sleep
+    ];
+    initrd.availableKernelModules = [
+      "nvme"
+      "sd_mod"
+      "usb_storage"
+      "xhci_pci"
+    ];
+  };
+
+  # enable xe driver
+  hardware.intelgpu.driver = "xe";
+}
---
+++ hosts/sjorapport/storage.nix
@@ -0,0 +1,37 @@
+_: {
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "1f1050fa";
+
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = [
+      "defaults"
+      "size=2G"
+      "mode=755"
+    ];
+  };
+
+  fileSystems."/nix" = {
+    device = "sjorapport/NixOS/nix";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/etc/ssh" = {
+    device = "sjorapport/NixOS/etc/ssh";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/persist" = {
+    device = "sjorapport/NixOS/persist";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/9249-C4FB";
+    fsType = "vfat";
+  };
+}
