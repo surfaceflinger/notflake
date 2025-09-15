a4f864677d5d618d37127723b0c530a4c9275700
nat <nat@nekopon.pl>
2025-09-15 16:00:25 +0200

    modules/nixos/common/zfs: fix double mounts

--- modules/nixos/common/zfs.nix
+++ modules/nixos/common/zfs.nix
@@ -11,12 +11,6 @@
     monthly = 2;
   };
 
-  boot.postBootCommands = lib.optionalString config.boot.zfs.enabled ''
-    # to debug check: journalctl -b | grep stage-2-init
-    echo Mounting all zfs filesystems
-    ${pkgs.zfs}/bin/zfs mount -a
-  '';
-
   # fix unmounting /var/log (implicit zfs native mountpoint) on shutdown
   systemd.services.systemd-journal-flush = {
     before = [ "shutdown.target" ];
