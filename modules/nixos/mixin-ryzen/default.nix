11ce21fa8ad434c2a135e57afdd4eb3fa499b86a
nat <nat@nekopon.pl>
2025-09-21 20:40:10 +0200

    modules/nixos/mixin-ryzen: don't blacklist k10temp

--- modules/nixos/mixin-ryzen/default.nix
+++ modules/nixos/mixin-ryzen/default.nix
@@ -3,7 +3,6 @@
   imports = [ "${inputs.nixos-hardware.result}/common/cpu/amd/pstate.nix" ];
 
   boot = {
-    blacklistedKernelModules = [ "k10temp" ];
     extraModulePackages = [ config.boot.kernelPackages.zenergy ];
     kernelModules = [
       "kvm-amd"
