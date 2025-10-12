756a965c8cdc7a9457b5ff97d5afb1cf97f71848
nat <nat@nekopon.pl>
2025-10-13 00:59:11 +0200

    hosts/jattelik/blahaj.pl/gotosocial: adapt metrics cfg to new release

--- hosts/jattelik/blahaj.pl/gotosocial.nix
+++ hosts/jattelik/blahaj.pl/gotosocial.nix
@@ -2,6 +2,7 @@ _:
 let
   account-domain = "blahaj.pl";
   host = "gts.${account-domain}";
+  metricsPort = "9464";
   port = 18740;
 in
 {
@@ -10,11 +11,17 @@ in
     settings = {
       inherit host port account-domain;
       advanced-rate-limit-requests = 1000;
-      bind-address = "[::1]";
+      bind-address = "::1";
       db-type = "sqlite";
       metrics-enabled = true;
     };
   };
+
+  systemd.services.gotosocial.serviceConfig.Environment = [
+    "OTEL_EXPORTER_PROMETHEUS_PORT=${metricsPort}"
+    "OTEL_METRICS_EXPORTER=prometheus"
+    "OTEL_METRICS_PRODUCERS=prometheus"
+  ];
 
   services.caddy.virtualHosts."${account-domain}".extraConfig = ''
     redir /.well-known/host-meta* https://${host}{uri} permanent
@@ -38,7 +45,7 @@ in
     logFormat = "output discard";
     extraConfig = ''
       encode zstd gzip
-      reverse_proxy * http://[::1]:${toString port} { flush_interval -1 }
+      reverse_proxy * http://127.0.0.1:${metricsPort}
     '';
   };
 }
