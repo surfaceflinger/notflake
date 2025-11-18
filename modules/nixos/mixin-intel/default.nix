51cf766f4689b2a3da9182a46fb233f6f42aa423
nat <nat@nekopon.pl>
2025-11-18 23:03:10 +0100

    modules/nixos/mixin-intel: remove zenergy
    reused mixin-ryzen and forgot abt this

--- modules/nixos/mixin-intel/default.nix
+++ modules/nixos/mixin-intel/default.nix
@@ -1,14 +1,10 @@
-{ config, ... }:
-{
+_: {
   # base
   nixpkgs.hostPlatform = "x86_64-linux";
 
   # kernel/modules/firmware
   hardware.enableRedistributableFirmware = true;
-  boot = {
-    extraModulePackages = [ config.boot.kernelPackages.zenergy ];
-    kernelModules = [ "kvm-intel" ];
-  };
+  boot.kernelModules = [ "kvm-intel" ];
 
   # misc
   services.hardware.openrgb.motherboard = "intel";
