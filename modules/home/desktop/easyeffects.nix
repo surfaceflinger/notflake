c2c6ed0fa39691e3bf594282762db247597f9ec0
nat <nat@nekopon.pl>
2025-11-27 00:07:56 +0100

    modules/home/desktop/easyeffects: fix for easyeffects 8

--- modules/home/desktop/easyeffects.nix
+++ modules/home/desktop/easyeffects.nix
@@ -2,7 +2,7 @@
 {
   services.easyeffects.enable = true;
 
-  xdg.configFile = {
+  xdg.dataFile = {
     "easyeffects/irs/google-pixel-buds-pro-autoeq.irs".source = pkgs.fetchurl {
       url = "https://github.com/jaakkopasanen/AutoEq/raw/3.0.0/results/Rtings/in-ear/Google%20Pixel%20Buds%20Pro/Google%20Pixel%20Buds%20Pro%20minimum%20phase%2048000Hz.wav";
       hash = "sha256-Oh4+QegYb2W1uWUTiBPOT95VOSyhFT/QSRahNxGDONs=";
