a3621e68ef228bcc57dd087874a61201f3bc425d
nat <nat@nekopon.pl>
2026-01-04 03:28:19 +0100

    modules/nixos/common/hardening: enable debugfs

--- modules/nixos/common/hardening.nix
+++ modules/nixos/common/hardening.nix
@@ -42,6 +42,7 @@
 
   boot.kernelParams = [
     "bdev_allow_write_mounted=0"
+    "debugfs=on" # reenable debugfs for some weird drivers and eg. rasdaemon
     "hash_pointers=always"
     "proc_mem.force_override=never"
   ];
