78bac365971efde458f25c4a04d3285d6ffa32a8
nat <nat@nekopon.pl>
2026-02-13 23:31:25 +0100

    hosts/jattelik/storage: fileshare fix

--- hosts/jattelik/storage.nix
+++ hosts/jattelik/storage.nix
@@ -3,4 +3,10 @@ _: {
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
 
   fileSystems."/boot".device = "/dev/vda1";
+
+  fileSystems."/srv/fileshare" = {
+    device = "jattelik/srv/fileshare";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
 }
